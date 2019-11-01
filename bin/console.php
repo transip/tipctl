196d25d95d6ae7cda51849e273ae96674757b6ab
Stefan König <skonig@transip.nl>
2019-11-01 11:54:47 +0100

    Added OperatingSystem resource + renamed AvailabilityZone to AvailabilityZones

--- bin/console.php
+++ bin/console.php
@@ -3,23 +3,25 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
-use Transip\Api\CLI\Command\AvailabilityZone;
+use Transip\Api\CLI\Command\AvailabilityZones;
 use Transip\Api\CLI\Command\MailService;
 use Transip\Api\CLI\Command\Products;
 use Transip\Api\CLI\Command\Vps;
 use Transip\Api\CLI\Command\Vps\Addon as VpsAddon;
 use Transip\Api\CLI\Command\Vps\Backup as VpsBackup;
 use Transip\Api\CLI\Command\Vps\IpAddress as VpsIpAddress;
+use Transip\Api\CLI\Command\Vps\OperatingSystem as VpsOperatingSystem;
 use Transip\Api\CLI\Command\Traffic;
 
 $app = new Application();
 $app->setName('Transip API CLI');
-$app->add(new AvailabilityZone());
+$app->add(new AvailabilityZones());
 $app->add(new Products());
 $app->add(new Vps());
 $app->add(new VpsAddon());
 $app->add(new VpsBackup());
 $app->add(new VpsIpAddress());
+$app->add(new VpsOperatingSystem());
 $app->add(new MailService());
 $app->add(new Traffic());
 
