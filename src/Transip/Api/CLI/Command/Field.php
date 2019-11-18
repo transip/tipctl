1b7fe4687d4eb04d2ae63ab8e628c8255d7a8052
Samih Soylu <ssoylu@transip.nl>
2019-11-18 14:36:05 +0100

    Implemented a field class for constants

---
+++ src/Transip/Api/CLI/Command/Field.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Transip\Api\CLI\Command;
+
+class Field
+{
+
+    const PRIVATENETWORK_NAME = 'PrivateNetworkName';
+    const PRIVATENETWORK_NAME__DESC = 'The private network name';
+    const PRIVATENETWORK_DESCRIPTION = 'PrivateNetworkDescription';
+    const PRIVATENETWORK_DESCRIPTION__DESC = 'The private network description';
+
+    const VPS_NAME = 'VpsName';
+    const VPS_NAME__DESC = 'The name of the vps';
+}
