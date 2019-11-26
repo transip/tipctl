a33edafbdbee8b25a4a862a15138c59d2f31b4e4
Samih Soylu <ssoylu@transip.nl>
2019-11-26 10:50:33 +0100

    Implemented bernardo's reflection class solution

--- bin/console.php
+++ bin/console.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Finder\Finder;
 use Transip\Api\CLI\Utilities\Strings;
 
@@ -12,20 +13,16 @@ $app->setName('Transip API CLI');
 $finder = new Finder();
 $finder->files()->in(__DIR__ . '/../src/Transip/Api/CLI/Command');
 
-$ignoreClass = ['Field', 'AbstractCommand'];
-
 foreach ($finder as $file) {
     $className = str_replace('.php', '', $file->getRelativePathname());
     $className = str_replace('/', '\\', $className);
 
-    $class = explode('/', $className);
-    if (in_array(end($class), $ignoreClass, true)) {
-        continue;
-    }
-
     $fullClassName = "Transip\Api\CLI\Command\\" . $className;
 
-    $app->add(new $fullClassName);
+    $reflectionClass = new ReflectionClass($fullClassName);
+    if ($reflectionClass->isInstantiable() && $reflectionClass->isSubclassOf(Command::class)) {
+        $app->add(new $fullClassName);
+    }
 }
 
 $app->run();
