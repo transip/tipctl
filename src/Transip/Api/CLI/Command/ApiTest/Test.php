032a7f7b99edfdef40f84931680f2dfbbb88d6a0
Stefan König <skonig@transip.nl>
2019-12-03 14:21:36 +0100

    Added Test resource

---
+++ src/Transip/Api/CLI/Command/ApiTest/Test.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Transip\Api\CLI\Command\ApiTest;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Transip\Api\CLI\Command\AbstractCommand;
+
+class Test extends AbstractCommand
+{
+    protected function configure(): void
+    {
+        $this->setName('ApiTest:test')
+            ->setDescription('Check your API connection and token via this simple test')
+            ->setHelp('will check if the API is reachable with token');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $success = $this->getTransipApi()->test()->test();
+        $this->output(['success' => $success]);
+    }
+}
