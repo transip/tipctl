e97397d0d7873c6d1cd9ba5d9364478d5afd504b
Stefan König <skonig@transip.nl>
2019-12-13 13:45:47 +0100

    Added OrderMultiple function to Vps

---
+++ src/Transip/Api/CLI/Command/Vps/OrderMultiple.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Transip\Api\CLI\Command\Vps;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Transip\Api\CLI\Command\AbstractCommand;
+use Transip\Api\CLI\Command\Field;
+
+class OrderMultiple extends AbstractCommand
+{
+    protected function configure(): void
+    {
+        $this->setName('Vps:orderMultiple')
+            ->setDescription('Order multiple new VPSs')
+            ->setHelp('Order multiple VPSs with this command. After the order process has been completed (payment will occur at a later stage should direct debit be used) the VPS will automatically be provisioned and deployed. Use Products:getAll to get a list of products')
+            ->addArgument(Field::VPS_MULTIPLE_COUNT, InputArgument::REQUIRED, Field::VPS_MULTIPLE_COUNT__DESC)
+            ->addArgument(Field::VPS_PRODUCT_NAME, InputArgument::REQUIRED, Field::VPS_PRODUCT_NAME__DESC)
+            ->addArgument(Field::VPS_OS_NAME, InputArgument::REQUIRED, Field::VPS_OS_NAME__DESC)
+            ->addArgument(Field::VPS_ADDONS, InputArgument::OPTIONAL, Field::VPS_ADDONS__DESC . Field::OPTIONAL, '')
+            ->addArgument(Field::VPS_HOSTNAME, InputArgument::OPTIONAL, Field::VPS_HOSTNAME__DESC . Field::OPTIONAL, '')
+            ->addArgument(Field::AVAILABILITY_ZONE, InputArgument::OPTIONAL, Field::AVAILABILITY_ZONE__DESC . Field::OPTIONAL, '')
+            ->addArgument(Field::VPS_DESCRIPTION, InputArgument::OPTIONAL, Field::VPS_DESCRIPTION__DESC . Field::OPTIONAL, '');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $count            = $input->getArgument(Field::VPS_MULTIPLE_COUNT);
+        $productName      = $input->getArgument(Field::VPS_PRODUCT_NAME);
+        $operatingSystem  = $input->getArgument(Field::VPS_OS_NAME);
+        $addons           = $input->getArgument(Field::VPS_ADDONS);
+        $hostname         = $input->getArgument(Field::VPS_HOSTNAME);
+        $availabilityZone = $input->getArgument(Field::AVAILABILITY_ZONE);
+        $description      = $input->getArgument(Field::VPS_DESCRIPTION);
+
+        $addons = (strlen($addons) > 1) ? explode(',', $addons) : [];
+
+        $vpss = [];
+
+        for($i=0; $i < $count; $i++) {
+            $vpss[] = [
+                "productName"      => $productName,
+                "operatingSystem"  => $operatingSystem,
+                "addons"           => $addons,
+                "hostname"         => $hostname,
+                "availabilityZone" => $availabilityZone,
+                "description"      => $description
+
+            ];
+        }
+
+        $this->getTransipApi()->vps()->orderMultiple($vpss);
+    }
+}
