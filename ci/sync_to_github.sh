c4d9b25b464333393e9e0743a600ed8dcceea0e2
samih soylu <ssoylu@transip.nl>
2020-02-19 10:48:23 +0100

    Added a deployment to GitHub pipeline
    Refactored to psr-4 folder structure
    
    Created a new stage for pipeline
    
    Removed username and token, and made a single credentials variable for the sync script.
    
    This was necessary because the secrets pipeline was failing.

---
+++ ci/sync_to_github.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -z "GITHUB_CREDENTIALS" ] ; then
+  echo "we need a github credentials in order to proceed";
+  exit 1;
+fi
+
+# Remove the default origin/HEAD -> origin/master ref
+# as github shows this as new branch
+git remote set-head origin -d
+
+git push --prune https://${GITHUB_CREDENTIALS}@github.com/transip/tipctl.git +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
