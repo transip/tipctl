480c1c9f13bbfe58577f9bdbb9ae46c4a3650078
Samih Soylu <ssoylu@transip.nl>
2020-05-01 10:36:46 +0200

    Added pagination to tipctl

--- src/Command/SSHKey/GetAll.php
+++ src/Command/SSHKey/GetAll.php
@@ -2,9 +2,11 @@
 
 namespace Transip\Api\CLI\Command\SSHKey;
 
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Transip\Api\CLI\Command\AbstractCommand;
+use Transip\Api\CLI\Command\Field;
 
 class GetAll extends AbstractCommand
 {
@@ -12,12 +14,17 @@ class GetAll extends AbstractCommand
     {
         $this->setName('sshkey:getall')
             ->setDescription('List all SSH keys in your account')
-            ->setHelp('');
+            ->addArgument(Field::PAGE, InputArgument::OPTIONAL, Field::PAGE__DESC . Field::OPTIONAL, 0)
+            ->addArgument(Field::ITEMS_PER_PAGE, InputArgument::OPTIONAL, Field::ITEMS_PER_PAGE__DESC . Field::OPTIONAL, 0)
+            ->setHelp('This command supports pagination, using this command you can limit the amount of ssh keys returned by the api call, which might be useful if you expect a lot of response objects and you want to spread that over to multiple requests.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $sshKeyList = $this->getTransipApi()->sshKey()->getAll();
+        $page = $input->getArgument(Field::PAGE);
+        $itemsPerPage = $input->getArgument(Field::ITEMS_PER_PAGE);
+
+        $sshKeyList = $this->getTransipApi()->sshKey()->getSelection($page, $itemsPerPage);
 
         $this->output($sshKeyList);
     }
