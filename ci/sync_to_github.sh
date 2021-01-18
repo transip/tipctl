e223f8df12f0181229839268e9ecb3ddb28d5ed6
Dion Bosschieter <dbosschieter@transip.nl>
2021-01-18 13:56:55 +0100

    Fix sync_to_github in shallow git checkout pipelines

--- ci/sync_to_github.sh
+++ ci/sync_to_github.sh
@@ -5,8 +5,16 @@ if [ -z "$GITHUB_CREDENTIALS" ] ; then
   exit 1;
 fi
 
-# Remove the default origin/HEAD -> origin/master ref
-# as github shows this as new branch
+if [ -z "$CI_REPOSITORY_URL" ] ; then
+  echo "we need a repository url to proceed";
+  exit 1;
+fi
+
+# force a full clone, instead of expecting a non shallow repository
+git clone $CI_REPOSITORY_URL /tmp/repository
+cd /tmp/repository
+
+# Remove the default origin/HEAD -> origin/master ref as github shows this as new branch
 git remote set-head origin -d
 
 git push --prune https://${GITHUB_CREDENTIALS}@github.com/transip/tipctl.git +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
