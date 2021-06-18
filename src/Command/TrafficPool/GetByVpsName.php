51a369daf8bd6ac66226e2406feba428080fe766
Tim Gooren <tgooren@transip.nl>
2021-06-18 12:13:18 +0000

    Deprecated Traffic endpoint in favor of TrafficPool endpoint

--- src/Command/TrafficPool/GetByVpsName.php
+++ src/Command/TrafficPool/GetByVpsName.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Transip\Api\CLI\Command\Traffic;
+namespace Transip\Api\CLI\Command\TrafficPool;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,16 +13,16 @@ class GetByVpsName extends AbstractCommand
 {
     protected function configure(): void
     {
-        $this->setName('traffic:getbyvpsname')
-            ->setDescription('Get traffic information for a VPS')
+        $this->setName('trafficpool:getbyvpsname')
+            ->setDescription('Get Traffic Pool information for a VPS')
             ->addArgument(Field::VPS_NAME, InputArgument::REQUIRED, Field::VPS_NAME__DESC)
-            ->setHelp('This command prints traffic information for a given vps.');
+            ->setHelp('This command prints Traffic Pool information for a given vps.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $vpsName = $input->getArgument(Field::VPS_NAME);
-        $traffic = $this->getTransipApi()->traffic()->getByVpsName($vpsName);
+        $traffic = $this->getTransipApi()->trafficPool()->getByVpsName($vpsName);
         $this->output($traffic);
     }
 }
