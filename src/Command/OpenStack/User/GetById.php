c11c9189b80282ca2bf4016ecd1294a97a90fc93
Dave Heineman <dheineman@transip.nl>
2021-11-10 13:22:58 +0100

    Add a function to get a openstack user by id

---
+++ src/Command/OpenStack/User/GetById.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Transip\Api\CLI\Command\OpenStack\User;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Transip\Api\CLI\Command\AbstractCommand;
+use Transip\Api\CLI\Command\Field;
+
+class GetById extends AbstractCommand
+{
+    protected function configure(): void
+    {
+        $this->setName('openstack:user:getbyid')
+            ->setDescription('Get the openstack user information')
+            ->addArgument(Field::OPENSTACK_USER_ID, InputArgument::REQUIRED, Field::OPENSTACK_USER_ID__DESC)
+            ->setHelp('This API call returns details for an openstack project');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $userId = $input->getArgument(Field::OPENSTACK_USER_ID);
+
+        $user = $this->getTransipApi()
+            ->openStackUsers()
+            ->getByUserId($userId);
+
+        $this->output($user);
+    }
+}
