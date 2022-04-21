34d611a2ecf4a53e3ec6b192665b5c58446ec6d6
Pierre van  Horick <phorick@transip.nl>
2022-04-21 09:18:37 +0000

    Add includes to cli

--- src/Command/Domain/GetByName.php
+++ src/Command/Domain/GetByName.php
@@ -4,6 +4,7 @@ namespace Transip\Api\CLI\Command\Domain;
 
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Transip\Api\CLI\Command\AbstractCommand;
 use Transip\Api\CLI\Command\Field;
@@ -15,13 +16,16 @@ class GetByName extends AbstractCommand
         $this->setName('domain:getbyname')
             ->setDescription('Get specific domain by its domainname')
             ->setHelp('Provide a name to retrieve your Domain by name')
-            ->addArgument(Field::DOMAIN_NAME, InputArgument::REQUIRED, Field::DOMAIN_NAME__DESC);
+            ->addArgument(Field::DOMAIN_NAME, InputArgument::REQUIRED, Field::DOMAIN_NAME__DESC)
+            ->addOption(Field::INCLUDE, null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, FIELD::INCLUDE__DESC);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $domainName = $input->getArgument(Field::DOMAIN_NAME);
-        $domain = $this->getTransipApi()->domains()->getByName($domainName);
+        $includes = $input->getOption(Field::INCLUDE) ?? [];
+
+        $domain = $this->getTransipApi()->domains()->getByName($domainName, $includes);
         $this->output($domain);
         return 0;
     }
