272bc0974511b77e87c5bb05438b8c0ad9a64b29
Roald Stolte <stolter@transip.nl>
2022-09-08 08:10:32 +0000

    Update email:maillist:update to allow for name change (name is a required argument)

--- src/Command/Email/MailList/Update.php
+++ src/Command/Email/MailList/Update.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Transip\Api\CLI\Command\AbstractCommand;
 use Transip\Api\CLI\Command\Field;
+use Transip\Api\Library\Entity\Email\MailList;
 
 class Update extends AbstractCommand
 {
@@ -18,6 +19,7 @@ class Update extends AbstractCommand
             ->setHelp('Update existing mail list in your account')
             ->addArgument(Field::EMAIL_MAIL_LIST_ID, InputArgument::REQUIRED, Field::EMAIL_MAIL_LIST_ID__DESC)
             ->addArgument(Field::EMAIL_DOMAIN_NAME, InputArgument::REQUIRED, Field::EMAIL_DOMAIN_NAME__DESC)
+            ->addArgument(Field::EMAIL_MAIL_LIST_NAME, InputArgument::REQUIRED, Field::EMAIL_MAIL_LIST_NAME__DESC)
             ->addArgument(Field::EMAIL_MAIL_LIST_ENTRIES, InputArgument::IS_ARRAY, Field::EMAIL_MAIL_LIST_ENTRIES__DESC);
     }
 
@@ -25,13 +27,18 @@ class Update extends AbstractCommand
     {
         $mailListId = $input->getArgument(Field::EMAIL_MAIL_LIST_ID);
         $domainName = $input->getArgument(Field::EMAIL_DOMAIN_NAME);
+        $name = $input->getArgument(Field::EMAIL_MAIL_LIST_NAME);
         $entries = $input->getArgument(Field::EMAIL_MAIL_LIST_ENTRIES);
 
-        $this->getTransipApi()->mailLists()->update(
-            $mailListId,
-            $domainName,
-            $entries
-        );
+        $mailList = $this->getTransipApi()->mailLists()->getByDomainNameAndId($domainName, $mailListId);
+
+        $mailList->setName($name);
+
+        if ($entries !== null) {
+            $mailList->setEntries($entries);
+        }
+
+        $this->getTransipApi()->mailLists()->update($mailListId, $domainName, $mailList);
         return 0;
     }
 }
