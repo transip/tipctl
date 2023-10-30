dc8fce927b5a33b521daaed200302d9db5cf4caa
Jordi Donadeu <donadeuj@transip.nl>
2023-10-30 10:38:58 +0000

    Block storage methods

--- src/Settings/Settings.php
+++ src/Settings/Settings.php
@@ -17,7 +17,7 @@ class Settings
 
     public const TRANSIP_API_ENDPOINT = 'https://api.transip.nl/v6';
 
-    public const TRANSIP_CLI_VERSION = '6.33.0';
+    public const TRANSIP_CLI_VERSION = '6.33.1';
 
     /**
      * @var string
